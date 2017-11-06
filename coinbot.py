eab751359d3b9afae55b60a91be92f6a14fb3c14
Alex O <crazyalex11@gmail.com>
2017-11-06 15:06:28 -0500

    Added Percentages
    Added 24hr percent change for each by default except all, which requires -p at the end. ex: !all -p

--- coinbot.py
+++ coinbot.py
@@ -18,48 +18,69 @@ async def on_message(message):
         await client.send_message(message.channel, '```Bitcoin Price !btc, !bitcoin\nEthereum Price !eth, !ethereum\nBoth !coins, !all ```')
     elif message.content.startswith(('!btc', '!bitcoin', '!Bitcoin')):
         price = '```Bitcoin: $'
-        price += coinPrice(1)
+        price += str(coinPrice(1)) + ' '
+        price += coinPercent(1) + '%'
         price += '```'
-        await client.send_message(message.channel, price)
-    elif message.content.startswith('!bye'):
-        await client.send_message(message.channel, '```GFY```')
-    elif message.content.startswith(('!eth','!ETH', '!ethereum', '!Ethereum')):
-        price = '```Ethereum: $'
-        price += coinPrice(2)
-        price += '```'
-        await client.send_message(message.channel, price)
-    elif message.content.startswith(('!ltc','!LTC', '!litecoin', '!LiteCoin')):
-        price = '```Litecoin: $'
-        price += coinPrice(3)
-        price += '```'
-        await client.send_message(message.channel, price)
-    elif message.content.startswith(('!coin', '!all', '!COIN', '!Coin')):
-        price = coinPrice(-1)
         await client.send_message(message.channel, price)
         
+    elif message.content.startswith('!bye'):
+        await client.send_message(message.channel, '```GFY```')
+        
+    elif message.content.startswith(('!eth','!ETH', '!ethereum', '!Ethereum')):
+        price = '```Ethereum: $'
+        price += str(coinPrice(2)) + ' '
+        price += coinPercent(2) + '%'
+        price += '```'
+        await client.send_message(message.channel, price)
+        
+    elif message.content.startswith(('!ltc','!LTC', '!litecoin', '!LiteCoin')):
+        price = '```Litecoin: $'
+        price += str(coinPrice(3)) + ' '
+        price += coinPercent(3) + '%'
+        price += '```'
+        await client.send_message(message.channel, price)
+        
+    elif message.content.startswith(('!coin', '!all', '!COIN', '!Coin')):
+        all = '```Bitcoin:   $'
+        all += str(coinPrice(1)) + '  ' 
+        if message.content.endswith('-p'):
+            all += coinPercent(1) + '%'
+        all += "\nEthereum:  $"
+        all += str(coinPrice(2)) + '   '
+        if message.content.endswith('-p'):
+            all += coinPercent(2) + '%'
+        all += '\nLitecoin:  $'
+        all += str(coinPrice(3)) + '    '
+        if message.content.endswith('-p'):
+            all += coinPercent(2) + '%'
+        all += '```'
+        await client.send_message(message.channel, all)
 
         
 def coinPrice( x ):
     if x == 1:
         current = float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/sell').json()['data']['amount']) + float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy').json()['data']['amount'])
         current = round(current/2,2)
-        return str(current)
+        return current
     elif x == 2:
         current = float(requests.get('https://api.coinbase.com/v2/prices/ETH-USD/sell').json()['data']['amount']) + float(requests.get('https://api.coinbase.com/v2/prices/ETH-USD/buy').json()['data']['amount'])
         current = round(current/2,2)
-        return str(current)
+        return current
     elif x ==3:
         current = float(requests.get('https://api.coinbase.com/v2/prices/LTC-USD/sell').json()['data']['amount']) + float(requests.get('https://api.coinbase.com/v2/prices/LTC-USD/buy').json()['data']['amount'])
         current = round(current/2,2)
+        return current
+   
+
+def coinPercent( x ):
+    if x == 1:
+        current = round(((coinPrice(1)/float(requests.get('https://api.gdax.com/products/BTC-USD/stats').json()['open']))-1)*100,2)
         return str(current)
-    else:
-        all = '```Bitcoin:   $'
-        all += coinPrice(1)
-        all += "\nEthereum:  $"
-        all += coinPrice(2)
-        all += '\nLitecoin:  $'
-        all += coinPrice(3)
-        all += '```'
-        return all
- 
+    elif x == 2:
+        current = round(((coinPrice(2)/float(requests.get('https://api.gdax.com/products/ETH-USD/stats').json()['open']))-1)*100,2)
+        return str(current)
+    elif x == 3:
+        current = round(((coinPrice(3)/float(requests.get('https://api.gdax.com/products/LTC-USD/stats').json()['open']))-1)*100,2)
+        return str(current)
+
 client.run(KEY)
--- testv.py
+++ testv.py
@@ -5,10 +5,20 @@ import sys
 from datetime import datetime
 
 
+
 sell_price = float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/sell').json()['data']['amount']) + float(requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy').json()['data']['amount'])
 sell_price = round(sell_price/2,2)
 stat = float(requests.get('https://api.gdax.com/products/BTC-USD/stats').json()['open'])
 print (sell_price)
 print(stat)
-print(round(100*((sell_price/stat)-1),2))
-print(round(100*((stat/sell_price)-1),2))
+current = round(((sell_price/float(requests.get('https://api.gdax.com/products/BTC-USD/stats').json()['open']))-1)*100,2)
+print(str(current))
+
+#test
+for x in range (0, 10):
+    try:
+        print(requests.get('https://api.stocktwits.com/api/2/streams/symbol/BTC.X.json').json()['messages'][x]['body'])
+        if str(requests.get('https://api.stocktwits.com/api/2/streams/symbol/BTC.X.json').json()['messages'][x]['entities']['sentiment']) != 'null':
+            print(requests.get('https://api.stocktwits.com/api/2/streams/symbol/BTC.X.json').json()['messages'][x]['entities']['sentiment'])
+    except ValueError:
+        print()
