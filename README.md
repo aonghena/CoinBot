c90c77bbbf5bc147baefd83856c092d588f7a1cf
Alex O <crazyalex11@gmail.com>
2017-11-18 10:23:56 -0500

    Added IEX Price
    Added Stock Ticker report.
    Cleaned misc

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # CoinBot
 This is a CryptoCurrency Bot for Discord.
-<br>
-Supports 900+ cryptocurrencies.
+Supports 1200+ cryptocurrencies.
 
 
-<br>
-<br>
-<br>
+
+
+
+
+#### Usage:<br>
+`@, !help:` Command Info
+
+
+`!all:` Bitcoin, ethereum, litecoin price/24hr change.
+
+
+`!news:` Return two of the most recent news articles related to cryptocurrency
+
+
+`!COINTICKER:` COINTICKER price/24 hr change<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Example: !BTC: Bitcoin price/24 hr change.
+  
+  
+`$STOCKTICKER:` STOCKTICKER price/24 hr change.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Example: $AAPL: Apple Share price/24 hr change
+  
+ ____________________________________________
 <a href="https://developers.coinbase.com/">Coinbase</a>
 <br>
 <a href="http://coincap.io">Coincap</a>
+<br>
+<a href="https://iextrading.com/">IEX</a>
--- coinbot.py
+++ coinbot.py
@@ -5,6 +5,21 @@ from password import KEY
 from decimal import *
 from coinNews import coinNews
 
+'''
+CoinBase/GDAX: Bitcoin/Etherum/Litcoin prices
+CoinMarketCap: Other CryptoCoins price
+IEXPrice: Stock ticker price
+google news rss feed: News
+
+#####
+
+Cryptonator, and CoinCap methods are 
+below, but not in use, If you would
+like to switch out CoinMarketCap data
+for one of them, just go to l 93ish
+to switch
+'''
+
 client = discord.Client()
 
 @client.event
@@ -12,16 +27,17 @@ async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('------')
+    await client.change_presence(game=discord.Game(name='Cipher'))
 
-
-    
 @client.event
 async def on_message(message):
-    if message.content.startswith('!help'):
-        await client.send_message(message.channel, '```!btc !bitcoin : to get the latest bitcoin price from coinbase \n!eth !ethereum : to get the latest Ethereum Price'+
-        '\n!ltc !litecoin : to get the latest etherum price from Coinbase: \n!all : get latest eth btc ltc price \n!COIN_TICKER : to get the latest price of that coin'+
-        '\n!news shows the latest cryptocurrency news```')
+    if message.content.lower().startswith(('!help','<@' + client.user.id + ">")):
+        help = '```!btc !bitcoin : to get the latest bitcoin price from coinbase \n!eth !ethereum : to get the latest Ethereum Price'
+        + '\n!ltc !litecoin : to get the latest etherum price from Coinbase: \n!all : get latest eth btc ltc price '
+        + '\n!COIN_TICKER : to get the latest price of the coin' 
+        + '\n$STOCK_TICKER : to get the latest price of the ticker'
+        + '\n!news shows the latest cryptocurrency news```'
+        await client.send_message(message.channel, help)
     elif message.content.startswith(('!btc', '!bitcoin', '!Bitcoin')):
         cost , change = coinBasePrice(1)
         price = '```Bitcoin: $'
@@ -30,17 +46,17 @@ async def on_message(message):
         price += '```'
         await client.send_message(message.channel, price)
     
-    elif message.content.startswith(('!news', '!News')):
+    elif message.content.lower().startswith('!news'):
         c, b = coinNews()
         news = str(c)
-        if message.content.endswith('-b'):
+        if message.content.lower().endswith('-b'):
             news = str(b)
         await client.send_message(message.channel, news)   
        
-    elif message.content.startswith('!bye'):
-        await client.send_message(message.channel, '```GFY```')
+    elif message.content.lower().startswith('!bye'):
+        await client.send_message(message.channel, '```Bye```')
         
-    elif message.content.startswith(('!eth','!ETH', '!ethereum', '!Ethereum')):
+    elif message.content.lower().startswith(('!eth', '!ethereum')):
         cost , change = coinBasePrice(2)
         price = '```Ethereum: $'
         price += str(cost) + ' '
@@ -48,7 +64,7 @@ async def on_message(message):
         price += '```'
         await client.send_message(message.channel, price)
         
-    elif message.content.startswith(('!ltc','!LTC', '!litecoin', '!LiteCoin')):
+    elif message.content.lower().startswith(('!ltc', '!litecoin')):
         cost , change = coinBasePrice(3)
         price = '```Litecoin: $'
         price += str(cost) + ' '
@@ -56,7 +72,7 @@ async def on_message(message):
         price += '```'
         await client.send_message(message.channel, price)
         
-    elif message.content.startswith(('!all')):
+    elif message.content.lower().startswith(('!all')):
         cost , change = coinBasePrice(1)
         all = '```Bitcoin:   $'
         all += str(cost) + '  ' 
@@ -72,11 +88,27 @@ async def on_message(message):
         all += '```'
         await client.send_message(message.channel, all)
             
+    #Gets price of all other cryptocurrencies 
     elif message.content.startswith(('!')):
         t = str(message.content[1:])
-        price = coinCapePrice(t.upper())
+        ############################
+        '''' currently not in use, but if you would like to switch out 
+        where you get your coin data from just un-comment the provider you want
+        and comment the previous used one
+        '''
+        #price = coinCapPrice(t.upper())
+        #price = cryptonatorPrice(t)
+        #############################
+
+        price = coinMarketCapPrice(t.upper())
         price += '```'
-        
+        await client.send_message(message.channel, price)
+
+    #Get price of stock ticker
+    elif message.content.startswith(('$')):
+        t = str(message.content[1:])
+        price = IEXPrice(t.upper())
+        price += '```'
         await client.send_message(message.channel, price)
 
 #coinbase/GDAX price       
@@ -95,13 +127,46 @@ def coinBasePrice(x):
     per = Decimal(per).quantize(TWOPLACES)
     return current, per
     
-        
-#CoinCap.io price
-def coinCapePrice(t):
+#coinmarketcap
+def coinMarketCapPrice(t):
+    loc = -1
+    coinInfo = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=1500').json()
+    for list in coinInfo:
+        if list['symbol'] == t:
+            loc = 0
+            coin = list['name']
+            cost = list['price_usd']
+            per = list['percent_change_24h']
+            break
+    if(loc == -1):
+        price = '```Ticker Not Found'
+        return price
+    price = '```' + str(coin) + ': $'
+    price += str(cost) + ' '
+    price += str(per)+'%'
+    return price
+
+#IEXPrice for Stocks
+def IEXPrice(t):
     try:
-        coin = requests.get('http://coincap.io/page/' + t).json()['display_name']
-        cost = requests.get('http://coincap.io/page/' + t).json()['price_usd']
-        per = float(requests.get('http://coincap.io/page/' + t).json()['cap24hrChange'])
+        stockInfo = requests.get('https://api.iextrading.com/1.0/stock/'+ (t)+  '/quote').json()
+        company = stockInfo['companyName']
+        cost = stockInfo['latestPrice']
+        per = stockInfo['change']
+        price = '```' + str(company) + ' $'
+        price += str(cost) + ' '
+        price += str(per) + '%'
+    except:
+        price = '```Ticker Not Found'
+    return price
+
+#CoinCap.io price
+def coinCapPrice(t):
+    try:
+        coinInfo = requests.get('http://coincap.io/page/' + t).json()
+        coin = coinInfo['display_name']
+        cost = coinInfo['price_usd']
+        per = float(coinInfo['cap24hrChange'])
         if(float(cost) > 1):
             cost = round(float(cost),2)
         price = '```' + str(coin) + ': $'
@@ -111,6 +176,20 @@ def coinCapePrice(t):
         price = '```Ticker Not Found'
 
     return price
-        
+    
+#cryptonator price
+def cryptonatorPrice(t):
+    try:
+        coinInfo = requests.get('https://api.cryptonator.com/api/ticker/' + (t)+ '-usd').json()
+        coin = coinInfo['ticker']['base']
+        cost = coinInfo['ticker']['price']
+        if(float(cost) > 1):
+            cost = round(float(cost),2)
+        price = '```' + str(coin) + ': $'
+        price += str(cost)
+    except:
+        price = '```Ticker Not Found'
+    return price
+
 
 client.run(KEY)
